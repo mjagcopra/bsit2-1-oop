ad61c91e33bc04e67f9c20e859ce0a93a1adb22e
Mike Janzen <mjagcopra21441@liceo.edu.ph>
2025-09-04 07:47:04 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       System.out.println("Main Branch");
+        System.out.println("Library Management System");
+        LibraryManager manager = new LibraryManager();
+
+        manager.showBooks();
+        manager.addBook();
+        manager.removeBook();
+
+        System.out.println("Program completed successfully.");
     }
 }
