5d63bf01b1ecedbe843ab96c8ee78e82ad0769fd
Mike Janzen <mjagcopra21441@liceo.edu.ph>
2025-09-15 10:18:01 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       System.out.println("Main Branch");
+
+        PetService services = new PetService();
+        System.out.println("Basic checkup: $" + services.calculateFee());
+        System.out.println("Checkup with vaccination: $" + services.calculateFee(true));
+        System.out.println("Full service: $" + services.calculateFee(true, true));
+        System.out.println("Emergency: $" + services.calculateFee("Trauma"));
+        System.out.println("\n");
+
+        System.out.println("Welcome to the Pet Clinic!");
+        System.out.println("============================");
+
+        Pet[] pets = {
+                new Dog("Buddy", 3),
+                new Cat("Whiskers", 2),
+                new Bird("Tweety", 1)
+        };
+
+        for (Pet pet : pets) {
+            pet.displayInfo();
+            pet.makeSound();
+            if(pet != pets[pets.length - 1]) System.out.println();
+        }
+        System.out.println("\n");
+
+        System.out.println("Training Session Started!");
+        System.out.println("======================");
+
+        for (Pet pet : pets) {
+            if (pet instanceof Trainable) {
+                ((Trainable) pet).performTrick();
+            }
+        }
     }
 }
